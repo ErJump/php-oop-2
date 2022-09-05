406c644b9d28bf8a800d9841897c5e9cb7853408
ErJump <fedelijunior@gmail.com>
2022-09-05 16:39:41 +0200

    fix: :bug: Fixes include path

--- Classes/Food.php
+++ Classes/Food.php
@@ -1,6 +1,6 @@
 <?php
     include_once __DIR__ . '/Product.php';
-    include_once __DIR__ . '../Traits/Seasonal.php';
+    include_once __DIR__ . '/../Traits/Seasonal.php';
     class Food extends Product{
         use Seasonal;
         protected $expirationDate;
